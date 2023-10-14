fbe58c663912cbce007821b4c6fa36913bcfac35
boneandrea <bone.andrea.anthrax@gmail.com>
2023-10-14 12:40:06 +0900

    update aboutpage layout

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,17 +1,23 @@
 <template>
+<div class="container">
   <h1>使い方</h1>
-  <h2>状態</h2>
-  <ul class="list-group">
-    <li class="list-group-item">白 予約なし</li>
-    <li class="list-group-item">青 予約あり</li>
-    <li class="list-group-item">赤 営業外</li>
-  </ul>
-  <h2>操作</h2>
-  <ul class="list-group">
-    <li class="list-group-item">白 タップ:予約あり</li>
-    <li class="list-group-item">青 タップ:予約なし</li>
-    <li class="list-group-item">青 ロングタップ:名前入力</li>
-    <li class="list-group-item">白 ロングタップ:営業外</li>
-    <li class="list-group-item">赤 ロングタップ:営業中</li>
-  </ul>
+  <section>
+    <h2>状態</h2>
+    <ul class="list-group">
+      <li class="list-group-item">白 予約なし</li>
+      <li class="list-group-item">青 予約あり</li>
+      <li class="list-group-item">赤 営業外</li>
+    </ul>
+  </section>
+  <section>
+    <h2>操作</h2>
+    <ul class="list-group">
+      <li class="list-group-item">白 タップ:予約あり</li>
+      <li class="list-group-item">青 タップ:予約なし</li>
+      <li class="list-group-item">青 ロングタップ:名前入力</li>
+      <li class="list-group-item">白 ロングタップ:営業外</li>
+      <li class="list-group-item">赤 ロングタップ:営業中</li>
+    </ul>
+  </section>
+</div>
 </template>
