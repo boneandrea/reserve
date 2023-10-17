4faf0ed01931347f21f5f9f3e082ec2e0b5e2a61
boneandrea <bone.andrea.anthrax@gmail.com>
2023-10-18 03:36:43 +0900

    Add calendar

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,11 +1,29 @@
 <template>
-<div class="container">
-  <h1>{{today}}</h1>
-  <div class="btn-group">
-      <button type="button" class="btn btn-outline-primary" @click="go(-1)">前日</button>
-      <button type="button" class="btn btn-outline-primary" @click="go(1)">翌日</button>
-    </div>
-  <table class="table">
+    <div class="container">
+        <div class="btn-group">
+            <button type="button" class="btn btn-outline-primary" @click="go(-1)">前日</button>
+            <button type="button" class="btn btn-outline-primary" @click="go(1)">翌日</button>
+            <div class="w-md-2 my-datepicker">
+                <VueDatePicker
+                    v-model="currentDate"
+                    :format="format"
+                    :enable-time-picker="false"
+                    :max-date="maxDate"
+                    week-start="0"
+                    ref="datepicker"
+                    locale="ja"
+                    placeholder="Jump..."
+                    select-text="Select"
+                    cancel-text="Cancel"
+                    now-button-label="現在を選択"
+                    :show-now-button="false"
+                    @update:model-value="select"
+                >
+                </VueDatePicker>
+            </div>
+        </div>
+
+        <table class="table">
             <thead>
                 <tr>
                     <th scope="col"></th>
@@ -23,9 +41,23 @@
         </table>
     </div>
 </template>
+<script>
+ export default {
+     name: 'TimeTable',
+     components: {
+         VueDatePicker
+     }
+ }
+</script>
 <script setup>
- import { ref } from 'vue'
- import Td from './Td.vue'
+ import { ref } from 'vue';
+ import Td from './Td.vue';
+
+ import VueDatePicker from '@vuepic/vue-datepicker';
+ import '@vuepic/vue-datepicker/dist/main.css'
+ const MAX_DATE=10
+ const today=new Date()
+ const maxDate=new Date(today.setDate(today.getDate()+MAX_DATE))
  const times=ref([
      "9:00",
      "9:30",
@@ -52,14 +84,32 @@
      "20:00",
      "20:30",
  ])
- const currentDate=new Date()
- let today=ref(`${currentDate.getFullYear()}/${currentDate.getMonth()+1}/${currentDate.getDate()} (${["日","月","火","水","木","金","土"][currentDate.getDay()]})`)
- defineProps(['times',"currentDate","today"])
+ const format="yyyy-MM-dd"
+ const currentDate=ref(new Date())
+ defineProps(['times'])
  function go(d){
-     currentDate.setDate(currentDate.getDate()+d)
-     today.value=`${currentDate.getFullYear()}/${currentDate.getMonth()+1}/${currentDate.getDate()} (${["日","月","火","水","木","金","土"][currentDate.getDay()]})`
+     if(!currentDate.value) return
+     const newDate=new Date(currentDate.value)
+     newDate.setDate(currentDate.value.getDate()+d)
+     currentDate.value=newDate
+     fetchAndUpdateTable()
+ }
+ function select(e){
+     currentDate.value=e
+     fetchAndUpdateTable()
+ }
+ function fetchAndUpdateTable(){
+     console.log(currentDate)
+     // kick API
  }
 </script>
 <style scoped>
  tr{ line-height: 200%}
+ .my-datepicker {
+     max-width: 250px;
+     margin: auto;
+ }
+ .dp__action_select {
+     width: 200px important!
+ }
 </style>
